76041d0c8790a6b52b42ae73e04423005b98f256
imagi <imagi.ffxiv@gmail.com>
2024-03-09 02:23:45 +0100

    6d8c1da

---
+++ js/about.fca6e1a6.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkkelart"]=self["webpackChunkkelart"]||[]).push([[443],{7381:function(n,t,a){a.r(t),a.d(t,{default:function(){return f}});var e=a(3396);const r={class:"about"},u=(0,e._)("h1",null,"This is an about page",-1),s=[u];function c(n,t){return(0,e.wg)(),(0,e.iD)("div",r,s)}var i=a(89);const l={},o=(0,i.Z)(l,[["render",c]]);var f=o}}]);
+//# sourceMappingURL=about.fca6e1a6.js.map
