d702296753af2377efb2f59c07ae75486f46a1f3
tmartin42 <thibaultmartin42@gmail.com>
2025-12-14 03:50:31 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<imagi>/<imagi-gallery>.git main:gh-pages
+git push -f git@github.com:imagiffxiv/imagi-gallery.git main:gh-pages
 
-cd -
+cd ..
